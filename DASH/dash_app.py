0cfe6b802edaa3a57a869c24d42804030d4f1aa7
Reslley Gabriel <reslleygabriel@gmail.com>
2020-05-19 22:39:29 -0300

    updated dash with new vision
    added new fields and graph, with new layout

--- DASH/dash_app.py
+++ DASH/dash_app.py
@@ -3,120 +3,170 @@ import dash
 from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 import pandas as pd
 
-df = pd.read_csv('../models/cellphones/cellphonedata.csv')
+df = pd.read_csv('../models/cellphones/cellphones_final_results.csv')
 
-app = dash.Dash()
-app.title = 'dynamic-pricing'
+# app = dash.Dash()
+app = dash.Dash(external_stylesheets=[dbc.themes.FLATLY])
+app.title = 'DS4A'
 
 # Boostrap CSS.
 # app.css.append_css({'external_url': 'https://codepen.io/amyoshino/pen/jzXypZ.css'})  
 
 # Materialize CSS
-external_css = ["https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css"]
-for css in external_css:
-    app.css.append_css({"external_url": css})
+# external_css = ["https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css"]
+# for css in external_css:
+#     app.css.append_css({"external_url": css})
 
-app.layout = html.Div(
-    html.Div([
-            html.Div([
-                    html.H1
-                    (
-                        children='Dynamic Pricing',
-                        className = "col s7 m7 l7",
-                        style={
-                            'text-align': 'center'
-                        }
-                    ),
-                    html.Img
-                    (
-                        src="https://vagas.byintera.com/wp-content/uploads/2020/03/logo-olist.png",
-                        className='col s5 m5 l5',
-                        style={
-                            'height': '15%',
-                            'width': '10%',
-                            'float': 'right',
-                            'position': 'relative',
-                            'margin-right': 50
 
-                        }
-                    )
-                ], className = "row"
+
+app.layout = dbc.Container(
+    html.Div(
+        [
+            html.Div(
+                [
+                    html.Div(
+                        html.H1
+                        (
+                            children='Olist Dynamic Pricing',
+                            style={
+                                'text-align': 'center',
+                                'margin-top': '15px',
+                                'margin-bottom': '25px'
+                            }
+                        )
+                    ) 
+                ]
             ),
 
-            html.Div([
-                    html.Div([
-                            html.P('Choose Price'),
-                            dcc.Checklist
-                            (
-                                    id = 'selector_choice',
-                                    options=[
-                                        {'label': 'Olist', 'value': 'Olist CellPhone Prices'},
-                                        {'label': 'Competition', 'value': 'Competition CellPhone Prices'},
-                                    ],
-                                    value=['Olist CellPhone Prices', '0'],
-                                    labelStyle={'display': 'inline-block'}
-                            ),
-#                             html.Div(
-#                                 [
-#                                     dcc.Dropdown(
-#                                         id='year',
-#                                         options= [{'label': str(item),
-#                                                    'value': str(item)}
-#                                                     for item in set(df['year'])],
-#                                         multi=True,
-#                                         value=list(set(df['year']))
-#                                     )
-#                                 ],
-#                                 className='col s6 m6 l6',
-#                                 style={'margin-top': '10'}
-#                             )
+            html.Div(
+                [
+                    html.Div(
+                        [   html.P('Product Group'),
+                            dcc.Dropdown(
+                                id='selector_group',
+                                options= [{'label': str(item),
+                                           'value': str(item)}
+                                            for item in set(df['group'])],
+                                multi=True,
+                                value=list(set(df['group']))
+                            )
                         ],
-                        className='col s6 m6 l6'
+                        className='col s4 m4 l4'
                     ),
-                ], className="row"
+                    html.Div(
+                        [   html.P('Product Type'),
+                            dcc.Dropdown(
+                                id='selector_type',
+                                options= [{'label': str(item),
+                                           'value': str(item)}
+                                            for item in set(df['type'])],
+                                multi=True,
+                                value=list(set(df['type']))
+                            )
+                        ],
+                        className='col s4 m4 l4'
+                    ),
+                    html.Div(
+                        [   html.P('Price Range'),
+                            dcc.Dropdown(
+                                id='selector_price_range',
+                                options= [{'label': str(item),
+                                           'value': str(item)}
+                                            for item in set(df['price_range'])],
+                                multi=True,
+                                value=list(set(df['price_range']))
+                            )
+                        ],
+                        className='col s4 m4 l4'
+                    )                      
+
+                ], className="row",
+                        style={
+                                'margin-top': '15px',
+                                'margin-bottom': '15px'
+                            }
             ),
 
-            html.Div([
+#             html.Div(
+#                 [
+
+#                     html.Div(
+#                         [   html.P('Baseline'),
+#                             dcc.Input(
+#                                 placeholder='Baseline',
+#                                 type='text',
+#                                 value=''
+#                             ) 
+#                         ],
+#                         className='col s4 m4 l4'
+#                     ),
+#                     html.Div(
+#                         [   html.P('RL Model'),
+#                             dcc.Input(
+#                                 placeholder='RL Model',
+#                                 type='text',
+#                                 value=''
+#                             ) 
+#                         ],
+#                         className='col s4 m4 l4'
+#                     ),
+#                     html.Div(
+#                         [   html.P('Delta'),
+#                             dcc.Input(
+#                                 placeholder='Delta',
+#                                 type='text',
+#                                 value=''
+#                             ) 
+#                         ],
+#                         className='col s4 m4 l4'
+#                     )                        
+
+#                 ], className="row",
+#                         style={
+#                                 'margin-top': '15px',
+#                                 'margin-bottom': '15px'
+#                             }
+#             ),
+
+
+            html.Div(
+                [
                     html.Div([
                             dcc.Graph(id='graph1'
                             )
-                        ], className= 'col s6 m6 l6'
+                        ], className= 'col s12 m12 l12'
                     )
 
                 ], className="row"
             )
-        ] 
-    ), className="container",
-    style={      
-        'margin-left':35,
-        'margin-right':35
-    }
+        ]
+    )
 )
 
 @app.callback(
     Output(component_id='graph1', component_property='figure'),
-    [Input(component_id='selector_choice', component_property='value')])
+    [Input(component_id='selector_group', component_property='value')])
 def update_graph(selector):
     data = []
-    if 'Olist CellPhone Prices' in selector:
-        data.append({'x': df.index, 'y': df.olist_price, 'type': 'line', 'name': 'Olist Price'})
-    if 'Competition CellPhone Prices' in selector:
-        data.append({'x': df.index, 'y': df.competition_price, 'type': 'line', 'name': 'Competition Price'})
+    if 'electronics' in selector:
+        data.append({'x': df.index, 'y': df.baseline_orders, 'type': 'line', 'name': 'Baseline Orders'})
+        data.append({'x': df.index, 'y': df.rl_orders, 'type': 'line', 'name': 'RL Orders'})
     figure = {
         'data': data,
         'layout': {
-            'title': 'Graph 1',
+            'title': 'Baseline x Reinforcement Learning',
             'xaxis' : dict(
-                title='x Axis',
+                title='Prices',
                 titlefont=dict(
                 family='Courier New, monospace',
                 size=20,
                 color='#7f7f7f'
             )),
             'yaxis' : dict(
-                title='y Axis',
+                title='Time',
                 titlefont=dict(
                 family='Helvetica, monospace',
                 size=20,
