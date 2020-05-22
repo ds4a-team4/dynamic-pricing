2b390f109ca6598f1e2a4f8129ed870d4aab5b6a
franciscomagioli <franciscomagioli@gmail.com>
2020-05-22 14:13:29 -0300

    Flask API

---
+++ back/flask_app.py
@@ -0,0 +1,41 @@
+import flask
+import jsonify
+from RL import create_results
+import pandas as pd
+from sqlalchemy import create_engine
+import secrets
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+def get_result_df():
+    uri = secrets.URI
+    engine = create_engine(uri)
+    df = pd.read_sql_table("rl_results", con=engine)
+    return df
+
+
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Dynamic Pricing API</h1><p>Endpoint list</p>"
+
+@app.route('/api/v1/dataframe/html', methods=['GET'])
+def api_dfhtml():
+    df = get_result_df()
+    return df.to_html(index = False)
+
+@app.route('/api/v1/dataframe/json', methods=['GET'])
+def api_dfjson():
+    df = get_result_df()
+    return flask.Response(df.to_json(orient="records"), mimetype='application/json')
+
+@app.route('/api/v1/process', methods=['GET'])
+def api_process():
+    create_results()
+    return "Processed"
+
+    
+
+app.run()
